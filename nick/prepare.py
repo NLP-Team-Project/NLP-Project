55e5a0304e2533b0ceab132e39825b50039ea199
Nickolas Pedri Miranda <nickmirandabr@gmail.com>
2023-10-23 14:11:00 -0500

    updated files

--- nick/prepare.py
+++ nick/prepare.py
@@ -5,6 +5,10 @@ import nltk
 from nltk.tokenize.toktok import ToktokTokenizer
 from nltk.corpus import stopwords
 
+from sklearn.model_selection import train_test_split
+
+import pandas as pd
+
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 
@@ -62,3 +66,65 @@ def cleanse(dataframe, col='', stemm=True, lem=True, extra_words=[], exclude_wor
     if lem:
         df['lemmatized'] = df.clean.apply(lemmatize)
     return df
+
+
+def train_val_test(df, strat='None', seed=100, stratify=False, print_shape=True):  # Splits dataframe
+    """ This function will split my data into train, validate and test. It has the option to stratify."""
+    if stratify:  # Will split with stratify if stratify is True
+        train, val_test = train_test_split(df, train_size=0.7, random_state=seed, stratify=df[strat])
+        val, test = train_test_split(val_test, train_size=0.5, random_state=seed, stratify=val_test[strat])
+        if print_shape:
+            print(train.shape, val.shape, test.shape)
+        return train, val, test
+    if not stratify:  # Will split without stratify if stratify is False
+        train, val_test = train_test_split(df, train_size=0.7, random_state=seed)
+        val, test = train_test_split(val_test, train_size=0.5, random_state=seed)
+        if print_shape:
+            print(f' train: {train.shape},  val: {val.shape},  test: {test.shape}')
+        return train, val, test
+
+
+def split_xy(df, target=''):
+    """This function will split x and y according to the target variable."""
+    x_df = df.drop(columns=target)
+    y_df = df[target]
+    return x_df, y_df  # Returns dataframe
+
+
+def replace_values_not_in_list(series, mylist, false_value):
+    new_values = []
+    for value in series:
+        if value in mylist:
+            new_values.append(value)
+        else:
+            new_values.append(false_value)
+    return new_values
+
+
+def top_languages(repo, n):
+    top_langs = list(repo.language.value_counts().index[:n])
+    filtered_languages = replace_values_not_in_list(repo.language, top_langs, 'Other')
+    return filtered_languages
+
+
+def word_counts(dataframe, col, data):
+    df = dataframe.copy()
+    uniques = df[col].unique()
+    col_names = ['all'] + list(df[col].unique())
+
+    word_count = []
+
+    all_words = df[data]
+    all_words = pd.Series(' '.join(all_words).split()).value_counts()
+    word_count.append(all_words)
+
+    for name in uniques:
+        words = df[data][df[col] == name]
+        words = pd.Series(' '.join(words).split()).value_counts()
+        word_count.append(words)
+
+    total_counts = pd.concat(word_count, axis=1) \
+        .set_axis(col_names, axis=1).fillna(0) \
+        .apply(lambda s: s.astype(int))
+
+    return total_counts
